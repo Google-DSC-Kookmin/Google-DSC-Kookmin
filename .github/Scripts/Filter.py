4b2877a10bc7b3725e33c7b4fc5547d5a9a28985
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2024-01-15 10:18:45 +0900

    fix: name, cover 예외처리

--- .github/Scripts/Filter.py
+++ .github/Scripts/Filter.py
@@ -7,7 +7,7 @@ def filtering(data):
     for page in datas:
         instance = {}
         
-        instance["Name"] = page["properties"]["이름"]["title"][0]["plain_text"]
+        instance["Name"] = checkIntroduction(page["properties"]["이름"]["title"])
         instance["Email"] = page["properties"]["E-mail"]["email"]
         instance["Position"] = getItems(page["properties"]["Role"]["multi_select"])
         instance["Responsibility"] = getItems(page["properties"]["Session"]["multi_select"])
@@ -20,15 +20,20 @@ def filtering(data):
     
     return json.dumps(result, ensure_ascii=False)
 
+
 def checkCover(data):
-    if data == None:
-        return None
-    return data["file"]["url"]
+    if data:
+        if data.get("type") == "file" and data.get("file"):
+            return data["file"]["url"]
+        elif data.get("type") == "external" and data.get("external"):
+            return data["external"]["url"]
+    
+    return None
 
 def checkIntroduction(data):
-    if data == []:
-        return None
-    return data[0]["plain_text"]
+    if data:
+        return data[0]["plain_text"]
+    return None
         
 def getItems(multi_select):
     items = []
